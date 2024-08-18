02b8691471a0200c1d65017d561bc970f11ab934
Alvaro zublime <131735140+AlvaroCRAK@users.noreply.github.com>
2024-08-18 16:39:01 -0500

    Add files via upload

---
+++ HashTable/Main.java
@@ -0,0 +1,18 @@
+public class Main{
+  public static void main(String[] args){
+    HashTable<String, Integer> hashTable = new HashTable<>();
+
+    hashTable.put("Alvaro", 1);
+    hashTable.put("Enrique", 2);
+    hashTable.put("Cencia", 3);
+
+    System.out.println(hashTable.get("Alvaro"));
+    System.out.println(hashTable.get("Enrique"));
+    System.out.println(hashTable.get("Cencia"));
+
+    hashTable.remove("Alvaro");
+    System.out.println(hashTable.get("Alvaro"));
+
+    System.out.println(hashTable.size());
+  }
+}
